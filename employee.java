c79303254066b72fac6cadefc4e784b8fa5671d0
singh-preeti <55255569+singh-preeti@users.noreply.github.com>
2024-07-05 10:00:27 +0530

    added addition function to the code

--- employee.java
+++ employee.java
@@ -4,5 +4,15 @@ class Employee{
   String emp_name="Punit" ;
 
   System.out.println(emp_id "" +emp_name);
+
+  public int addition(int a,int b){
+    int c= a+b;
+      System.out.println(c);
+    return c;
+  }
+  public static void main(String args[]){
+    Employee emp = new Employee();
+    emp.addition(10,10);
+  }
   
 }
